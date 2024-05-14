7a651a730ed66b3771de55cc2085bcab0b76712d
Victor Marelle <victormarelle45@gmail.com>
2024-05-14 23:37:27 +0200

    création des filtres

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -9,13 +9,14 @@ async function logWorks() {
 async function logCategories() {
   const response = await fetch("http://localhost:5678/api/categories");
   const categories = await response.json();
-  console.log(categories);
+  return categories;
 }
 
-async function DOMFactory() {
-  const works = await logWorks();
+async function DOMFactory(works) {
   let gallery = document.querySelector(".gallery");
+
   gallery.innerHTML = "";
+
   works.forEach((element) => {
     let figure = document.createElement("figure");
     let img = document.createElement("img");
@@ -29,6 +30,55 @@ async function DOMFactory() {
   });
 }
 
-logCategories();
+async function DOMFilter() {
+  const works = await logWorks();
+  const categories = await logCategories();
 
-DOMFactory();
+  let filter = document.querySelector(".filter");
+  let tous = document.createElement("div");
+
+  categories.forEach((element) => {
+    let category = document.createElement("div");
+
+    category.innerHTML = element.name;
+    category.classList.add("button");
+
+    category.addEventListener("click", function (event) {
+      let target = event.target;
+
+      let button = document.querySelectorAll(".button");
+      button.forEach((el) => {
+        el.classList.remove("selected");
+      });
+
+      target.classList.add("selected");
+
+      const result = works.filter(
+        (el) => el.category.name === target.innerText
+      );
+      DOMFactory(result);
+    });
+
+    filter.appendChild(category);
+  });
+
+  tous.addEventListener("click", function (event) {
+    let target = event.target;
+
+    let button = document.querySelectorAll(".button");
+    button.forEach((el) => {
+      el.classList.remove("selected");
+    });
+
+    target.classList.add("selected");
+    DOMFactory(works);
+  });
+
+  tous.classList.add("button");
+  tous.innerHTML = "Tous";
+  filter.insertBefore(tous, filter.firstChild);
+}
+
+DOMFilter();
+logCategories();
+DOMFactory(await logWorks());
